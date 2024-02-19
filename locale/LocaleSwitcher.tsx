d0ef143023a68de7ad1757bbcfe942abc4acce3f
Goduu <idcangussu@hotmail.com>
2024-02-19 18:16:09 +0100

    Update AuthorLayout and LocaleSwitcher components

--- locale/LocaleSwitcher.tsx
+++ locale/LocaleSwitcher.tsx
@@ -53,14 +53,14 @@ export const LocaleSwitcher: FC = () => {
           {languageFlags[locale]}
           {locale}
         </button>
-        <div className={`z-10 ${!open && "hidden"} absolute bg-white divide-y divide-slate-100 rounded-s-lg shadow dark:bg-slate-700 bottom-full sm:top-full`}>
-          <ul className="py-2 text-sm" aria-label="language-button">
+        <div className={`z-10 ${!open && "hidden"} absolute bottom-full sm:top-full`}>
+          <ul className=" text-sm bg-white divide-y divide-slate-100 dark:divide-slate-500 rounded-s-lg shadow dark:bg-slate-700" aria-label="language-button">
             {locales.map((language) => (
               <li key={language}>
                 <button
                   type="button"
                   aria-label={language}
-                  className="inline-flex w-full px-4 py-2 text-sm text-slate-700 hover:bg-slate-100 dark:text-slate-400 dark:hover:bg-slate-600 dark:hover:text-white"
+                  className="inline-flex w-full rounded-s-lg px-4 py-2 text-sm text-slate-700 hover:bg-slate-100 dark:text-slate-400 dark:hover:bg-slate-600 dark:hover:text-white"
                   onClick={() => handleChangeLocale(language)}
                 >
                   <div className="inline-flex items-center gap-2">
